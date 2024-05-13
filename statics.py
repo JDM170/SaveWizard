2d40c51f9c734e1403986463856568e605cceb20
Lev Rusanov <30170278+JDM170@users.noreply.github.com>
2024-05-13 19:29:21 +0700

    Update statics.py
    Signed-off-by: Lev Rusanov <30170278+JDM170@users.noreply.github.com>

--- statics.py
+++ statics.py
@@ -2,5 +2,5 @@
 # -*- coding: utf-8 -*-
 
 update_config_name = "update.cfg"
-github_link = "https://raw.githubusercontent.com/JDM170/SaveWizard/master/"
+github_link = "https://raw.githubusercontent.com/JDM170/SaveWizard/configs/"
 hash_chunk_size = 4096
