f19a76d95d6dc012fd54e876ec1a68846009a134
Lev Rusanov <30170278+JDM170@users.noreply.github.com>
2024-12-06 19:00:34 +0700

    Fix config editor script
    Signed-off-by: Lev Rusanov <30170278+JDM170@users.noreply.github.com>

--- module_config_editor/script.py
+++ module_config_editor/script.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QResizeEvent, QCloseEvent, QClipboard
 from PyQt5.QtWidgets import QMainWindow, QFileDialog, QMessageBox, QApplication
 from ast import literal_eval
